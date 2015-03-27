f8d02bdd7482378b4db97a54bbf871a69d3edb93
MaximeBonnet27 <maximebonnet27@gmail.com>
2015-03-27 07:42:39 +0100

    Continué la factory, un peu

--- src/warcraft/ui/sprites/ImageFactory.java
+++ src/warcraft/ui/sprites/ImageFactory.java
@@ -10,7 +10,14 @@ package warcraft.ui.sprites;
 public class ImageFactory {
 
 	private static String TERRAIN_FILE = "resources/sprites_terrain.png";
+	private static String ORC_FILE = "resources/sprites_orc.png";
+	private static String MINE_FILE = "resources/sprites_mine.png";
+	private static String VILLAGEOIS_FILE = "resources/sprites_villageois.png";
+	
 	private static TileSet terrainTileSet = new TileSet(TERRAIN_FILE);
+	private static TileSet orcTileSet = new TileSet(ORC_FILE);
+	private static TileSet mineTileSet = new TileSet(MINE_FILE);
+	private static TileSet villageoisTileSet = new TileSet(VILLAGEOIS_FILE);
 	
 	/**
 	 * 
